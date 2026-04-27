54ef99228da7fb1f43301158014809445a2dfa92
miskaa <office.misko@gmail.com>
2026-04-27 22:41:52 +0200

    initial commit

---
+++ index.hpp
@@ -0,0 +1,26 @@
+/*
+    mUtils (created by miskaa)
+    mUtils is a collection of utilities for Geode modding
+
+    miskaas geode utils
+*/
+
+#include <Geode/Geode.hpp>
+using namespace geode::prelude;
+
+/**
+    includes for files;
+     - qol is basically "useful" stuff
+     - string utilities for text manipulation
+
+    * @file delayedtask.hpp;    @brief basically just wait
+    * @file string/helper.hpp;  @brief string formatting and truncation
+    * @file string/parser.hpp;  @brief color parsing and conversion
+*/
+
+#include "qol/delayedtask.hpp"
+#include "string/helper.hpp"
+#include "string/parser.hpp"
+
+
+// The Unlicense license (do anything you want with this code loll)
